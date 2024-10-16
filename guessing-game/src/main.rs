2758e0ff038535f1eecb22bd15abd4bbca03ec2c
Andrew Cabrera <andrewcabrera@MacBookPro.attlocal.net>
2024-10-15 19:30:28 -0700

    feature(game): added basic functionality to the game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("number secret is: {secret_number}");
+
+    loop {
+        println!("enter your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("There was an issue reading the line");
+
+        // convert to number to match secret number
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your guess was: {guess}");
+
+        // handle compare options
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
